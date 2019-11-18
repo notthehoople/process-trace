87b56c911e1aac1e54b953c711943679f1a69f02
Mark Bradshaw <notthehoople@googlemail.com>
2019-11-18 21:05:19 +0000

    Working parse. Now to add network connectivity

--- process-trace.go
+++ process-trace.go
@@ -13,41 +13,55 @@ func processInput(debug bool) {
 	var lowerBucket, upperBucket, cpuLatency int
 	var timeCounter int = 0
 
-	// Read in the Input
+	// Setup at process start
 
 	bucketValueMap := make(map[int]int)
-	inputArray := make([]string, 0)
 	scanner := bufio.NewScanner(os.Stdin)
-	for timeCounter = 0; timeCounter < 16; {
-		// Scans a line from Stdin(Console)
-		scanner.Scan()
-		// Holds the string that scanned
-		readText := scanner.Text()
-		if len(readText) != 0 {
-			if readText == "@usecs:" {
-				fmt.Println("We found usecs!")
-				timeCounter++
-			} else {
-				readResult, _ = fmt.Sscanf(readText, "[%d, %d) %d", &lowerBucket, &upperBucket, &cpuLatency)
-				if readResult > 0 {
-					fmt.Println("READ: ", readText)
-					fmt.Println("lowerBucket: ", lowerBucket)
-					fmt.Println("upperBucket: ", upperBucket)
-					fmt.Println("cpuLatency: ", cpuLatency)
 
-					bucketValueMap[lowerBucket] += cpuLatency
+	// We want the service to run forever, so loop forever
+	for {
+
+		for timeCounter = 0; timeCounter < 16; {
+			// Scans a line from Stdin(Console)
+			scanner.Scan()
+			// Holds the string that scanned
+			readText := scanner.Text()
+			if len(readText) != 0 {
+				if readText == "@usecs:" {
+					fmt.Println("We found usecs!")
+					timeCounter++
 				} else {
-					fmt.Println("FAILED: ", readText)
+					readResult, _ = fmt.Sscanf(readText, "[%d, %d) %d", &lowerBucket, &upperBucket, &cpuLatency)
+					if readResult > 0 {
+						fmt.Println("READ: ", readText)
+						fmt.Println("lowerBucket: ", lowerBucket)
+						fmt.Println("upperBucket: ", upperBucket)
+						fmt.Println("cpuLatency: ", cpuLatency)
+
+						bucketValueMap[lowerBucket] += cpuLatency
+					} else {
+						fmt.Println("FAILED: ", readText)
+					}
 				}
 			}
-
-			inputArray = append(inputArray, readText)
-			//		} else {
-			//			break
 		}
+
+		// When we get here, we've read and processed 15 different inputs so it's time to summarise
+		fmt.Println("timeCounter is:", timeCounter)
+		fmt.Println(bucketValueMap)
+
+		// Send the details to Graphite
+		// Send data
+		// Check number of times we've sent data to Graphite
+		// If it's around an hour, close the network connection and reopen it
+
+		// Now we've delivered our data, clear the map and let's go again
+		for k := range bucketValueMap {
+			delete(bucketValueMap, k)
+		}
+		timeCounter = 0
 	}
-	fmt.Println("timeCounter is:", timeCounter)
-	fmt.Println(bucketValueMap)
+
 }
 
 // Main routine
